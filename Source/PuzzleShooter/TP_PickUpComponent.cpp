b7ae22cc61ed298c4a6bbb8743c7500e7ec72708
Wait2Late <w82l8.islam@gmail.com>
2024-02-02 12:10:30 +0100

    Made it easier for the player to pick up with EndOverlap. Small QoL.

--- Source/PuzzleShooter/TP_PickUpComponent.cpp
+++ Source/PuzzleShooter/TP_PickUpComponent.cpp
@@ -6,6 +6,10 @@ UTP_PickUpComponent::UTP_PickUpComponent()
 {
 	// Setup the Sphere Collision
 	SphereRadius = 32.f;
+
+	OnComponentEndOverlap.AddDynamic(this, &UTP_PickUpComponent::OnOverlapEnd);
+	
+	
 }
 
 void UTP_PickUpComponent::BeginPlay()
@@ -13,7 +17,7 @@ void UTP_PickUpComponent::BeginPlay()
 	Super::BeginPlay();
 
 	// Register our Overlap Event
-	OnComponentBeginOverlap.AddDynamic(this, &UTP_PickUpComponent::OnSphereBeginOverlap);
+	// OnComponentBeginOverlap.AddDynamic(this, &UTP_PickUpComponent::OnSphereBeginOverlap);
 }
 
 void UTP_PickUpComponent::OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
@@ -24,8 +28,27 @@ void UTP_PickUpComponent::OnSphereBeginOverlap(UPrimitiveComponent* OverlappedCo
 	{
 		// Notify that the actor is being picked up
 		OnPickUp.Broadcast(Character);
+		OnPickUp.RemoveAll(this);
 
 		// Unregister from the Overlap Event so it is no longer triggered
 		OnComponentBeginOverlap.RemoveAll(this);
+		OnComponentEndOverlap.RemoveAll(this);
+	}
+}
+
+void UTP_PickUpComponent::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	// Checking if it is a First Person Character overlapping
+	APuzzleShooterCharacter* Character = Cast<APuzzleShooterCharacter>(OtherActor);
+	if(Character != nullptr)
+	{
+		// Notify that the actor is being picked up
+		OnPickUp.Broadcast(Character);
+
+		OnPickUp.RemoveAll(this);
+		// Unregister from the Overlap Event so it is no longer triggered
+		OnComponentBeginOverlap.RemoveAll(this); 
+		OnComponentEndOverlap.RemoveAll(this);
 	}
 }
--- Source/PuzzleShooter/TP_PickUpComponent.h
+++ Source/PuzzleShooter/TP_PickUpComponent.h
@@ -31,4 +31,12 @@ protected:
 	/** Code for when something overlaps this component */
 	UFUNCTION()
 	void OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+    void OnOverlapEnd(
+    	UPrimitiveComponent* OverlappedComp,
+    	AActor* OtherActor,
+    	UPrimitiveComponent* OtherComp,
+    	int32 OtherBodyIndex);
+
 };
